0299b899c8c0deda8dc9358128a1b800e5e6826b
= <tompurnell100@gmail.com>
2026-03-14 17:25:49 +0000

    Update page.tsx

--- stumble/app/page.tsx
+++ stumble/app/page.tsx
@@ -4,15 +4,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans items-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-10 px-13 bg-zinc-50 sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-neutral-700 items-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-10 px-13 bg-neutral-700 sm:items-start">
 
-        <Image src="/Stumble.svg" alt="Stumble Logo" width={200} height={200} className="mb-10" />
-        <h1 className="text-9xl center text-center w-full mb-20">stumble</h1>
+        <Image src="/StumbledWithText.svg" alt="Stumble Logo" width={200} height={200} className="my-10 w-full center" />
 
-        <Link href="/pubDate" className="flex flex-row w-full h-20 justify-center items-center text-3xl font-bold rounded-xl bg-gray-200 py-5 mb-20">
-            Swipe time
-        </Link>
+        <div className="flex flex-col w-full items-center gap-10">
+          <Link href="/pubDate" className = "flex flex-row w-full h-20 justify-center items-center text-3xl font-bold rounded-xl bg-yellow-200 text-neutral-700 py-5 ">
+            Make a new pub crawl
+          </Link>
+          <Link href="/pubCrawl" className = "flex flex-row w-full h-20 justify-center items-center text-3xl font-bold rounded-xl bg-yellow-200 text-neutral-700 py-5">
+            View a pub crawl
+          </Link>
+        </div>
+        
 
       </main>
     </div>
