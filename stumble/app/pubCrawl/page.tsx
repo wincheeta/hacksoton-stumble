3ec97ecc7af0aa8d9316fa4f13cfe2d83470e9fa
tombash766 <tompurnell100@gmail.com>
2026-03-15 04:25:53 +0000

    style

--- stumble/app/pubCrawl/page.tsx
+++ stumble/app/pubCrawl/page.tsx
@@ -70,9 +70,9 @@ export default function PubCrawl() {
       };
 
   return (
-    <div className="inline-flex w-full min-h-screen items-center justify-center bg-neutral-700 items-center">
+    <div className="inline-flex w-full min-h-screen justify-center bg-neutral-700 items-start">
       <RouteViewer order={optimisedPubs}></RouteViewer>
-      <main className="flex gap-10 min-h-screen w-full max-w-3xl flex-col items-center justify-start py-10 px-13 bg-neutral-700 sm:items-start">      
+      <main className="flex gap-10 min-h-screen w-full max-w-3xl flex-col items-start justify-start py-10 px-13 bg-neutral-700 sm:items-start">      
         <button 
           onClick={handleShowRoute} 
           className="flex flex-row w-full h-20 justify-center items-center text-3xl font-bold rounded-xl bg-yellow-200 text-neutral-700 py-5"
--- stumble/app/pubCrawl/routeViewer.tsx
+++ stumble/app/pubCrawl/routeViewer.tsx
@@ -7,7 +7,7 @@ export default function RouteViewer({ order } : { order : number[] }) {
     const {choices, setChoices} = useContext(ChoiceContext);
 
     return (
-        <div className="flex min-h-screen max-w-3xl flex-col items-center justify-start py-10 px-13 bg-neutral-700 sm:items-start gap-5">
+        <div className="flex max-w-3xl flex-col bg-neutral-700 sm:items-start gap-5 h-120 overflow-auto my-43 rounded-lg pr-5">
             { order.map( (x,i) => ( <div key={i} className="flex flex-row bg-yellow-200 sm:items-start p-6 justify-start rounded-lg"> Pub {i + 1}: {pubs[choices[x]].name} </div> ) ) }
         </div>
     )
